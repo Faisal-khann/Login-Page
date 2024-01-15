cd276cf7213387d40f51e31e17916a1223d07326
Faisal-Khann <faisalkhanfaisal919@gmail.com>
2024-01-16 00:38:36 +0530

    Add Initial Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let signupBtn = document.getElementById("signupBtn");
+let signinBtn = document.getElementById("signinBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
+
+//Adding click function
+signinBtn.onclick = function () {
+    nameField.style.maxHeight = "0";
+    title.innerHTML = "Sign In";
+    signupBtn.classList.add("disable");
+    signinBtn.classList.remove("disable");
+}
+signupBtn.onclick = function () {
+    nameField.style.maxHeight = "60px";
+    title.innerHTML = "Sign Up";
+    signupBtn.classList.remove("disable");
+    signinBtn.classList.add("disable");
+}
